c256d11719b64734b9362d6889678d689680c177
LICASKJS <licasmateusdasilva25@gmail.com>
2025-09-30 09:10:50 -0300

    Atualizar configuração para PostgreSQL e e-mail no Render

--- config.py
+++ config.py
@@ -2,12 +2,13 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key-here'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///fornecedores.db'
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://usuario:senha@localhost:5432/fornecedores_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     MAIL_SERVER = 'smtp.office365.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'notificacaosuprimentos@engeman.net'  
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or '02082023Ll*' 
-    
-    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_USERNAME') or 'notificacaosuprimentos@engeman.net'  
+    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_USERNAME') or 'notificacaosuprimentos@engeman.net'
